ce926a3da0bf6227d0cabfa03fc242263e80bedc
java-coders <samanta_arnab2006@yahoo.co.in>
2016-04-13 00:45:36 +0530

    fetching stations data

--- Station_CFF_Validation/src/com/station/cff/database/DataBaseUtility.java
+++ Station_CFF_Validation/src/com/station/cff/database/DataBaseUtility.java
@@ -2,7 +2,9 @@ package com.station.cff.database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 
 /**
@@ -42,4 +44,88 @@ public class DataBaseUtility {
 		}
 		return connection; 
 	}
+	
+	/**
+	 * 
+	 * @param query
+	 * @param connection
+	 * @return ResultSet
+	 */
+	public static ResultSet executeQuery(String query,Connection connection){
+		ResultSet rs = null;
+		try( Statement stmt =  connection.createStatement();) {
+			rs = stmt.executeQuery(query);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return rs;
+	}
+	
+	
+	/**
+	 * 
+	 * @param query
+	 * @param connection
+	 * @param elementToReturn
+	 * @return String
+	 */
+	public static String executeQuery(String query,Connection connection,int elementToReturn){
+		ResultSet rs = null;
+		String rtnString = "";
+		try( Statement stmt =  connection.createStatement();) {
+			rs = stmt.executeQuery(query);
+			while(rs.next()){
+				rtnString=rs.getString(elementToReturn);
+				
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return rtnString;
+	}
+
+	/**
+	 * 
+	 * @param query
+	 * @param connection
+	 * @return integer
+	 */
+	public static int executeInsert(String query,Connection connection){
+		try( Statement stmt =  connection.createStatement();) {
+			return stmt.executeUpdate(query);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return -1;
+	}
+	
+	/**
+	 * 
+	 * @param query
+	 * @param connection
+	 * @return integer
+	 */
+	public static int executeDelete(String query,Connection connection){
+		try( Statement stmt =  connection.createStatement();) {
+			return stmt.executeUpdate(query);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return -1;
+	}
+	/**
+	 * 
+	 * @param query
+	 * @param connection
+	 * @return integer
+	 */
+	public static int executeUpdate(String query,Connection connection){
+		try( Statement stmt =  connection.createStatement();) {
+			return stmt.executeUpdate(query);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return -1;
+	}
+
 }
--- Station_CFF_Validation/src/com/station/cff/validation/cff_validation.java
+++ Station_CFF_Validation/src/com/station/cff/validation/cff_validation.java
@@ -1,9 +1,33 @@
 package com.station.cff.validation;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.station.cff.database.DataBaseUtility;
+
 public class cff_validation {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		try {
+			Connection connection = DataBaseUtility.getConnection();
+			/** It will fetch the stations table **/
+			String query="SELECT * FROM FIRSTSPIN.STATIONS";
+			ResultSet resultSet = DataBaseUtility.executeQuery(query, connection);
+			while(resultSet.next()){
+				
+				System.out.println(resultSet.getString(1));
+				System.out.println(resultSet.getString(2));
+				System.out.println(resultSet.getString(3));
+				System.out.println(resultSet.getString(4));
+				
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
